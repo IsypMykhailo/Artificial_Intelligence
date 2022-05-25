66f0c07d8137d049627ade73b2fa0af0b4a3e912
IsypMykhailo <amazonkanameforme@gmail.com>
2022-05-25 19:49:58 +0200

    model

--- AI/AI/Form1.Designer.cs
+++ AI/AI/Form1.Designer.cs
@@ -28,12 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.wRed = new System.Windows.Forms.TextBox();
+            this.wYellow = new System.Windows.Forms.TextBox();
+            this.wGreen = new System.Windows.Forms.TextBox();
+            this.wResult = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(34, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(186, 174);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Цвет сфетофора";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.wGreen);
+            this.groupBox2.Controls.Add(this.wYellow);
+            this.groupBox2.Controls.Add(this.wRed);
+            this.groupBox2.Location = new System.Drawing.Point(273, 38);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(186, 174);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Веса";
+            // 
+            // wRed
+            // 
+            this.wRed.Location = new System.Drawing.Point(19, 39);
+            this.wRed.Name = "wRed";
+            this.wRed.Size = new System.Drawing.Size(111, 27);
+            this.wRed.TabIndex = 2;
+            // 
+            // wYellow
+            // 
+            this.wYellow.Location = new System.Drawing.Point(19, 72);
+            this.wYellow.Name = "wYellow";
+            this.wYellow.Size = new System.Drawing.Size(111, 27);
+            this.wYellow.TabIndex = 3;
+            // 
+            // wGreen
+            // 
+            this.wGreen.Location = new System.Drawing.Point(19, 105);
+            this.wGreen.Name = "wGreen";
+            this.wGreen.Size = new System.Drawing.Size(111, 27);
+            this.wGreen.TabIndex = 4;
+            // 
+            // wResult
+            // 
+            this.wResult.Location = new System.Drawing.Point(498, 110);
+            this.wResult.Name = "wResult";
+            this.wResult.Size = new System.Drawing.Size(111, 27);
+            this.wResult.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(17, 42);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(92, 24);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Красный";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(17, 75);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 24);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Желтый";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(17, 108);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(91, 24);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Зеленый";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.wResult);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox groupBox2;
+        private TextBox wGreen;
+        private TextBox wYellow;
+        private TextBox wRed;
+        private TextBox wResult;
     }
 }
